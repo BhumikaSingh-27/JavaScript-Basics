da492f217d042a50ebd5fc4456121c2fd8ed0508
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2026-03-19 17:29:16 +0530

    feat: solved reduce questions

---
+++ functionalPrograming/map.js
@@ -0,0 +1,69 @@
+// 1. Write an ES6 function that takes in an array of objects representing sounds and returns an array of sounds name with all the letters capitalized.
+
+// `*// Your ES6+ code here*const sounds = [  {    name: 'rain',    sound: 'tap tap tap',  },  {    name: 'fire',    sound: 'blaze',  },  {    name: 'water',    sound: 'slosh slosh',  },]console.log(capitalizeLetters(sounds)) *// Output: ["RAIN", "FIRE", "WATER"]*`
+
+// const sounds = [  {    name: 'rain',    sound: 'tap tap tap',  },  {    name: 'fire',    sound: 'blaze',  },  {    name: 'water',    sound: 'slosh slosh',  }];
+
+// const capitalizedNames = (sound) => {
+//     return sound.map(({name}) => name[0].toUpperCase()+name.slice(1)) //use slice when we need to extract few letters from a string
+// }
+
+// console.log(capitalizedNames(sounds))
+
+//==============================================================2=================================
+
+// 2. Write an ES6 function that takes an array of objects with name, price and quantity, and returns a new array with only the names containing **`bread`**
+
+// `*// Your ES6+ code here*const products = [  { name: 'Bread', price: 480, quantity: 3 },  { name: 'Clips', price: 200, quantity: 5 },  { name: 'green Bread Knife', price: 3077, quantity: 1 },  { name: 'Slipper', price: 150, quantity: 2 },]console.log(filterByKeyword(products))*// Output: ["Bread", "green Bread Knife"]*`
+
+// const products = [
+//   { name: "Bread", price: 480, quantity: 3 },
+//   { name: "Clips", price: 200, quantity: 5 },
+//   { name: "green Bread Knife", price: 3077, quantity: 1 },
+//   { name: "Slipper", price: 150, quantity: 2 },
+// ];
+
+// const filterNames = (products) => {
+//   return products.filter(({ name }) => name.toLowerCase().includes("bread")).map(({name})=>name);
+
+// };
+// console.log(filterNames(products));
+//==============================================================3=============
+// 3. Write an ES6 function that takes an array of objects with name, price and quantity, and returns the new array having only names of products who are having a quantity of more than 10 and a name of more than 5 characters in length.
+
+//     `*// Your ES6+ code here*const products = [  { name: 'Bread', price: 150, quantity: 20 },  { name: 'Hoodie', price: 200, quantity: 50 },  { name: 'Pyjama', price: 307, quantity: 10 },  { name: 'Slipper', price: 480, quantity: 30 },]console.log(filterProducts(products)) *// Output: ["Hoodie", "Slipper"]*`
+
+// const products = [
+//   { name: "Bread", price: 150, quantity: 20 },
+//   { name: "Hoodie", price: 200, quantity: 50 },
+//   { name: "Pyjama", price: 307, quantity: 10 },
+//   { name: "Slipper", price: 480, quantity: 30 },
+// ];
+
+// const filterProducts = (products) => {
+//   return products
+//     .filter(({ name, quantity }) => name.length >= 5 && quantity > 10)
+//     .map(({ name }) => name);
+// };
+// console.log(filterProducts(products));
+
+//================================================4==================================
+// 4. Write an ES6 function that takes an array of objects with name, salary and role, and returns a new array with names whose salary is an even number.
+
+//     `*// your ES6+ code here*const employees = [  { name: 'Raju', salary: 154, role: 'dev' },  { name: 'Aakash', salary: 200, role: 'dev' },  { name: 'Ramesh', salary: 3077, role: 'pm' },  { name: 'Jiten', salary: 487, role: 'qa' },]console.log(filterBySalary(employees)) *// Output: ["Raju", "Aakash"]*`
+
+// const employees = [
+//   { name: "Raju", salary: 154, role: "dev" },
+//   { name: "Aakash", salary: 200, role: "dev" },
+//   { name: "Ramesh", salary: 3077, role: "pm" },
+//   { name: "Jiten", salary: 487, role: "qa" },
+// ];
+
+// const filterBySalary = (users) => {
+//   return users.filter(({ salary }) => salary % 2 === 0).map(({ name }) => name);
+// };
+
+// console.log(filterBySalary(employees));
+//========================================================6===================================
+
+
---
+++ functionalPrograming/reduce.js
@@ -0,0 +1,132 @@
+// 5. Write an ES6 function that takes in an array of numbers and returns the sum of all the even numbers. Use the nullish coalescing operator to handle undefined or null values.
+
+//     `*// Your ES6+ code here*console.log(sumOfEvenNumbers([1, 2, 3, 4, 5, 6])) *// 12*console.log(sumOfEvenNumbers([1, 3, 5])) *// 0*`
+
+// const sumOfEvenNumbers = (array) =>{
+// return ( array ?? []).reduce(((acc,cur)=> cur%2===0?acc+cur:acc),0)
+// }
+
+// console.log(sumOfEvenNumbers([1, 3, 5]))
+
+// return (array ?? []).reduce((acc,cur)=>{
+//     acc=acc+cur;
+//     return acc
+// },0)
+// }
+// console.log(sumOfEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+//--------------------2------------------
+// Multiply all numbers
+
+// const multiplyAll = (arr) => {
+//   return arr.reduce((mul, cur) => mul * cur, 1 );
+// };
+// console.log(multiplyAll([1, 2, 3, 4]));
+
+//==============================3=============
+
+// const findMaxNumber = (arr) =>{
+//     return arr.reduce((max,cur)=>max>cur?max:cur,1)
+// }
+// console.log(findMaxNumber([3, 7, 9,2]))
+
+//===========================4=====================
+
+//minimum number
+// const  findMinimum = (arr) =>{
+//     return arr.reduce((min,cur)=> min<cur?min:cur,1)
+// }
+// console.log(findMinimum([2,1,4,5]))
+
+///===============count total elements==================
+
+// const arr = ["a", "b", "c"];
+
+// const countElement = (arr) => {
+//   return arr.reduce((total, cur) => (cur ? total + 1 : total), 0);
+// };
+// console.log(countElement(arr));
+
+///===================INTERMIDIATE LEVEL==========
+//*****************SUM OF EVEN NUMBERS ******
+// const num =[1, 2, 3, 4, 6];
+// // → 12
+// const sumOfEvenNumbers=(arr)=>{
+//     return arr.reduce((sum,cur)=>cur%2===0?sum+cur:sum,0)
+// }
+// console.log(sumOfEvenNumbers(num))
+
+//==============7. Flatten an array (1 level)==========
+// const nestedArr = [[1, 2], [3, 4]];
+// //  → [1, 2, 3, 4]
+// const flattenedArray = (arr) =>{
+//     return arr.reduce((flatArr,cur)=>  [...flatArr,...cur] ,[])
+// }
+
+// console.log(flattenedArray(nestedArr));
+
+//==============count occurences================
+// const aplhaArray = ["a", "b", "a", "c", "b", "a"];
+// → { a: 3, b: 2, c: 1 }
+
+// const countOccurence = (arr) =>{
+//with for loop
+//    const obj={};
+//    for(let l of arr){
+//     if(Object.keys(obj).includes(l)){
+//         obj[l]=obj[l]+1;
+//     }else{
+//         obj[l]=1
+//     }
+//    }
+//    return obj
+// }
+//with reduce
+// return arr.reduce((obj,cur)=>{
+// if(Object.keys(obj).includes(cur)){
+//     obj[cur]=obj[cur]+1
+// }else{
+//     obj[cur]=1
+// }
+// return obj
+// },{})
+
+//best version of the solution which is clean and optimal o(1)
+// return arr.reduce((obj,cur)=>{
+//     obj[cur]=(obj[cur]||0)+1;
+//     return obj},{})
+// }
+
+// console.log(countOccurence(aplhaArray))
+//==========================REMOVE DUPLICATE=============
+// const arr =[1, 2, 2, 3, 4, 4]
+// → [1, 2, 3, 4]
+
+// const removeDup =(arr)=>{
+//     const a=[];
+//     for(let i of arr){
+//         if(a.includes(i)) continue;
+//         else{
+//             a.push(i)
+//         }
+//     }
+//     return a
+// }
+//using reduce
+// const removeDup = (arr) =>{
+//     return arr.reduce((uniArr,cur)=>uniArr.includes(cur)? uniArr : [...uniArr,cur],[])
+// }
+// console.log(removeDup(arr))
+//============================### 13. Longest word
+
+const greetings = ["hi", "hello", "worldss"];
+const findLongestString = (arr) => {
+  let longStr = "";
+  for (let greet of arr) {
+    if (greet.length > longStr.length) {
+      longStr = greet;
+    }
+  }
+  return longStr;
+};
+console.log(findLongestString(greetings));
